25a6af56fb3776b4d2b8d0905b5ec7a9efd3eed3
Alexis Rojas <rojas.estrada19@gmail.com>
2022-02-24 21:30:41 -0600

    second commit

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const SocketIO = require('socket.io');
+const SocketIO = require("socket.io");
 console.log(SocketIO);
 
 // settings
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
 // files static
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 // start server
-const server = app.listen(app.get('port'), () => {
-    console.log('Server online http://localhost:' + app.get('port'));
-})
+const server = app.listen(app.get("port"), () => {
+  console.log("Server online http://localhost:" + app.get("port"));
+});
 
 // websockets init
 const io = SocketIO(server);
 
 // websockets
-io.on('connection', (socket) => {
-    console.log('new connection', socket.id);
-})
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+
+  socket.on("chat:message", (data) => {
+    io.sockets.emit("chat:message", data);
+    console.log(data);
+  });
+
+  socket.on("chat:typing", (data) => {
+    console.log(data);
+  });
+
+  socket.on("chat:typing", (data) => {
+      socket.broadcast.emit('chat:typing', data);
+  });
+});
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,35 @@
 // io('http://dominio');
 
 const socket = io()
+
+// DOM elements
+let message = document.getElementById('message')
+let username = document.getElementById('username')
+let button = document.getElementById('send')
+let output = document.getElementById('output')
+let actions = document.getElementById('actions')
+
+button.addEventListener('click', () => {
+    socket.emit('chat:message', {
+        user: username.value,
+        msg: message.value
+    })
+    console.log(username.value, message.value);
+})
+
+message.addEventListener('keypress', ()=>{
+    socket.emit('chat:typing', username.value);
+    console.log(username.value);
+})
+
+socket.on('chat:message', (data)=>{
+    console.log(data);
+    actions.innerHTML = ''
+    output.innerHTML += `<p>
+        <strong>${data.user}</strong>: ${data.msg}
+    </p>`
+})
+
+socket.on('chat:typing', (data)=>{
+    actions.innerHTML = `<p><em>${data} is typing...</em></p>`
+})
